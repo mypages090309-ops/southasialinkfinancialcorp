e85b88c995a3aa261ffb31c5fd5081863dba5b4e
mypages090309-ops <mypages.090309@gmail.com>
2026-03-25 13:30:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
+const API_URL = "https://safcworker.bagongacctt.workers.dev/";
+
+document.getElementById("form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  alert("Application Submitted! We'll contact you soon.");
+
+  const data = {
+    name: document.getElementById("name").value,
+    phone: document.getElementById("phone").value,
+    loan: document.getElementById("loan").value
+  };
+
+  try {
+    await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    alert("✅ Application submitted!");
+    document.getElementById("form").reset();
+
+  } catch (err) {
+    alert("❌ Error");
+  }
 });
+
+function calc() {
+  let a = document.getElementById("amount").value;
+  let m = document.getElementById("months").value;
+
+  if (!a || !m) {
+    document.getElementById("result").innerText = "Fill all fields";
+    return;
+  }
+
+  let monthly = (a * 1.2) / m;
+
+  document.getElementById("result").innerText =
+    "Monthly: ₱" + monthly.toFixed(2);
+}
