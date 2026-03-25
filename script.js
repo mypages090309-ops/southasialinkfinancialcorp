98190686c96d6d0d7b738a2f14b39df01b5c2c8f
mypages090309-ops <mypages.090309@gmail.com>
2026-03-25 13:51:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-const API_URL = "https://safcworker.bagongacctt.workers.dev/";
-
-document.getElementById("form").addEventListener("submit", async (e) => {
+document.getElementById("form").addEventListener("submit", function(e){
   e.preventDefault();
-
-  const data = {
-    name: document.getElementById("name").value,
-    phone: document.getElementById("phone").value,
-    loan: document.getElementById("loan").value
-  };
-
-  await fetch(API_URL, {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(data)
-  });
-
-  alert("✅ Application Submitted!");
+  alert("Application Submitted!");
 });
